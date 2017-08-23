039fa1b9ab6f3dbac6862c50ffb0110d4c64da78
Corey Cooley <corey.a.cooley@gmail.com>
2017-08-23 17:02:02 -0400

    First Commit

---
+++ ClassDemo/ClassDemo/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClassDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int IHateTylerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int TotalCreditHours { get; set; }
+    }
+}
